50b6b552784646ad07f4cf0f4bcf9fff2850532f
Katelyn Zhang <krzhang@ucsb.edu>
2019-11-17 04:02:01 -0800

    kz - created EqSearch.java according to instructions from lab

---
+++ src/main/java/hello/EqSearch.java
@@ -0,0 +1,30 @@
+package hello;
+
+public class EqSearch {
+
+    private int distance;
+    private int minmag;
+
+    public EqSearch() {
+        // constructor body
+    }
+
+    // getters
+    public int getDistance() {
+        return distance;
+    }
+
+    public int getMinmag() {
+        return minmag;
+    }
+
+    // setters
+    public void setDistance(int d) {
+        distance = d;
+    }
+
+    public void setMinmag(int m) {
+        minmag = m;
+    }
+
+}
